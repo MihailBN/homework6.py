537e08ebd39caae210def732152e643fd5d73844
MihailBN <aratparosan@gmail.com>
2024-06-13 12:39:19 +0500

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,13 @@
+my_dict = {'Вася': 1975, 'Егор': 1999, 'Маша': 2002}
+print(my_dict)
+print(my_dict['Маша'])
+print(my_dict.get('ваня'))
+print(my_dict['Егор'])
+del my_dict ['Егор']
+my_dict.update({'Камила': 1981, 'Артём': 1915})
+print(my_dict)
+
+my_set = {1, 'яблоко', 3.14}
+print(my_set)
+print(my_set.add((1, 2, 3 )))
+print(my_set)
